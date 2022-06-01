d33624a87b8be625f946c147d4ca1287623f4e51
Archie Fan <archiefan@gmail.com>
2022-06-01 11:46:15 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodingStringDemo
+{
+    internal class Class1
+    {
+		int[] brackets = new int[30];
+		public string DecodeString(string s)
+		{
+			Stack<int> stack = new Stack<int>();
+			for (int i = 0; i < s.Length; i++)
+			{
+				var c = s[i];
+				if (c == '[')
+					stack.Push(i);
+				else if (c == ']')
+					brackets[stack.Pop()] = i;
+			}
+
+			return Helper(s, 0, s.Length - 1);
+		}
+
+		private string Helper(string s, int start, int end)
+		{
+			string res = "";
+			while (start <= end)
+			{
+				var c = s[start++];
+				if (Char.IsLetter(c))
+					res += c;
+				else if (Char.IsDigit(c))
+				{
+					int num = int.Parse(c.ToString());
+					while (s[start] != '[')
+					{
+						int n;
+						if (int.TryParse(s[start++].ToString(), out n))
+						{
+							num *= 10;
+							num += n;
+						}
+					}
+					var inner = Helper(s, start + 1, brackets[start] - 1);
+					for (int i = 0; i < num; i++)
+						res += inner;
+					start = brackets[start] + 1;
+				}
+			}
+
+			return res;
+		}
+
+		static void Main()
+		{
+			Class1 sol = new Class1();
+			string s1 = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
+			Console.WriteLine(sol.DecodeString(s1));
+		}
+
+	}
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+public class Solution
+{
+    
+    public string DecodeString(string s)
+    {
+        StringBuilder finalstr = new StringBuilder();
+        StringBuilder multiplier = new StringBuilder();
+        StringBuilder multiplicand = new StringBuilder();
+        StringBuilder Getmultiplicand = new StringBuilder();
+        bool start_getting_multiplicand = false;
+        int count_parentheses = 0;
+        
+        int index = 1;
+        foreach (char c in s)
+        {
+            if (!start_getting_multiplicand)
+            {
+                if (char.IsDigit(c))
+                {
+                    multiplier.Append(c);
+                }
+                else if (c.ToString() == "[")
+                {
+                    start_getting_multiplicand = true;
+                    count_parentheses++;
+                }
+                else if (c.ToString() != "]")
+                {
+                    finalstr.Append(c);
+                }
+            } else
+            {
+                if (c.ToString() == "]" && count_parentheses==1)
+                {
+                    Getmultiplicand.Append("]");
+                    Solution sol = new Solution();
+                    multiplicand.Append(sol.DecodeString(Getmultiplicand.ToString()));
+                    if (multiplier.ToString() != "")
+                    {
+                        for (int i = 0; i < int.Parse(multiplier.ToString()); i++)
+                        {
+                            finalstr.Append(multiplicand);
+                        }
+                    }
+                    else
+                    {
+                        finalstr.Append(multiplicand);
+                    }
+                    multiplier.Clear();
+                    multiplicand.Clear();
+                    Getmultiplicand.Clear();
+                    start_getting_multiplicand = false;
+                    count_parentheses = 0;
+                }
+                else {
+                    if (c.ToString() == "[")
+                    {
+                        count_parentheses++;
+                    } else if (c.ToString() == "]")
+                    {
+                        count_parentheses--;
+                    }
+                    Getmultiplicand.Append(c);
+                }
+            }
+            index++;
+        }
+        return finalstr.ToString();
+    }
+
+    static void Main()
+    {
+        // 3[a]2[bc]
+        // 3[a2[c]]
+        // 2[abc]3[cd]ef
+        // 3[z]2[2[y]pq4[2[jk]e1[f]]]ef
+
+        Solution sol = new Solution();
+        string s1 = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
+        Console.WriteLine(sol.DecodeString(s1));
+
+        string s2 = "3[a2[c2[d]]]";
+        Console.WriteLine(sol.DecodeString(s2));
+
+        string s3 = "2[abc]3[cd]ef";
+        Console.WriteLine(sol.DecodeString(s3));
+    }
+}
